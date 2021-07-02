54cb08238bdcc80e8f9ae61a8ae7e940eafb1c60
dcrhodes <sanooke@me.com>
2021-07-02 17:02:26 -0400

    adjusts images for the cards

--- js/app.js
+++ js/app.js
@@ -7,16 +7,22 @@ const cards = document.querySelectorAll('.all-cards')
 let hasCardFlipped = false 
 let frozen = false
 let firstCard, secondCard
-
+let doh = new Audio('/sounds/doh.mp3')
+let woohoo = new Audio('/sounds/woohoo.mp3')
 
 
 
 // <----------------------- LISTENERS ----------------------->
 cards.forEach(card => card.addEventListener('click', flip))
 
-
-
-
+//this listener invokes a reload function to refresh the page when clicked
+document.getElementById('reset').addEventListener('click', function(){
+    location.reload()
+})
+document.getElementById('mute').addEventListener('click', function(){
+    doh = new Audio('/sounds/doh.mp3').muted
+    woohoo = new Audio('/sounds/woohoo.mp3').muted
+})
 
 // <----------------------- FUNCTIONS ----------------------->
 function flip() {
@@ -34,26 +40,43 @@ function flip() {
         hasCardFlipped = false
         secondCard = this
 
-    // checks to see if flipped cards match. removes flipped class so that they cannot be turned back over
+    // checks to see if flipped cards match. removes event listener so that they cannot be turned back over
     if (firstCard.dataset.char === secondCard.dataset.char){
         firstCard.removeEventListener('click', flip)
         secondCard.removeEventListener('click', flip)
-        //resets the board to default variable states after cards are revealed that don't match.
-        frozen = false
-        hasCardFlipped = false
-        firstCard = null
-        secondCard = null
+        // testing rollout animation
+        // firstCard.classList.add('animate__rollOut')
+        // secondCard.classList.add('animate__rollOut')
+        //
+        woohoo.play()
+        reset()
     } else {
         frozen = true
         setTimeout(() => {
+            doh.play()    
+        }, 300);
+        
+        setTimeout(() => {
             firstCard.classList.remove('flipped')
             secondCard.classList.remove('flipped')
-            //resets the board to default variable states after cards are revealed that don't match.
-            frozen = false
-            hasCardFlipped = false
-            firstCard = null
-            secondCard = null
+            reset()
         }, 1000);
     }
     }
 }
+
+//resets the board to default variable states when called.
+function reset(){
+    frozen = false
+    hasCardFlipped = false
+    firstCard = null
+    secondCard = null
+}
+
+//this function assigns a number 1-36 to each card and randomizes their order within the board div.  wrapping this function in parantheses cause it to become immediately invoked when the page is loaded
+(function scramble(){
+    cards.forEach(card => {
+        let random = Math.floor(Math.random() * 36)
+        card.style.order = random
+    })
+})();
